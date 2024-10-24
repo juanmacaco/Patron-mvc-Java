167435d2267fdf8b1c768f5fbd0b966142bfe2ed
dyedi <jmcantalejoperez@gmail.com>
2024-10-24 17:44:39 +0200

    patron.01

--- src/com/ecodeup/view/ClienteView.java
+++ src/com/ecodeup/view/ClienteView.java
@@ -2,7 +2,7 @@ package com.ecodeup.view;
 
 public class ClienteView {
 	public void imprimirDatosCliente(int id,String nombre, String apellido) {
-		System.out.println("**** DATOS CLIENTE ****");
+		System.out.println("**** DATOS MIOS ****");
 		System.out.println("Id: "+id);
 		System.out.println("Nombre: "+nombre);
 		System.out.println("Apellido: "+apellido);
